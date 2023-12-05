2a25678873577298998e7b291e55597ccf6ddb1d
jade <jadejung95@gmail.com>
2023-12-05 20:29:07 +0900

    create an input box and a button

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ### Install React app
 
 npx create-react-app [project name]
+
+### Components
+
+All of the built-in browser components support some props and events.
+
+- <input>
+    - onChange: An Event handler function. Required for controlled inputs. Fires immediately when the input’s value is changed by the user (for example, it fires on every keystroke). Behaves like the browser input event.
--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+//1. an input box and a button
+//2. enter values in the input box and click the button to add items
+//3. a delete button
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <input
+        type="text"
+        onChange={(event) => console.log(event.target.value)}
+      />
+      {/* event.target.value : update the state variable on any edits */}
+      <button>add</button>
+    </main>
   );
 }
 
